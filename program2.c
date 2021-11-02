d253c4b9150f62097be1dbc81f5dccd902db6812
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2021-11-02 18:15:23 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,17 @@
+
+
+
+#include<stdio.h>
+ int main()
+ {
+   int x,y,z;
+   printf("enter first number\n");
+   scanf("%d",&x);
+   printf("enter second number\n");
+   scanf("%d",&y);
+   z= x + y;
+   printf("addition of two number:%d",z);
+   return 0;
+   }
+   
+
